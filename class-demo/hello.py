f4c05ab01092ecb2c907391c652dbe5723573d72
Tunevert-alt <tunevert@aol.com>
2025-04-08 07:58:19 -0400

    this is webdeves class code

---
+++ class-demo/hello.py
@@ -0,0 +1,29 @@
+# This line is running python code without a variable
+print("hello world")
+print("I am learning python")
+
+
+# Working with a variable
+first_name = "Richard"
+last_name = "Oladimeji"
+
+# Concatenation (concatenating a variable)
+print (first_name + " " + last_name)
+
+
+
+# Types of variables
+# Integers (Whole numbers) e.g 0 - 9
+# Floating point/float (decimal numbers) e.g 0.1
+# String (str) - Text data ("hello", "python")
+# Boolean "bool" - True/false values
+
+
+# New sets of variables
+age = 21
+sentence = "is the legal age to buy alcohol in stores in"
+country = "America"
+
+full_sentence = sentence + " " + country
+
+print(str(age) + " " + full_sentence)
